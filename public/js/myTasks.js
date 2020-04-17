2058b117e6d437082cdd38e9ec96aab3ce9136ff
Lukas Hirsch <hirschlukasa@gmail.com>
2020-04-16 22:25:35 -0500

    rendering todo list seems complete need to add functionality to buttons

--- public/js/myTasks.js
+++ public/js/myTasks.js
@@ -11,29 +11,30 @@ $(document).ready(function () {
             console.log(data)
             const taskHolder = $('.taskHolder')
 
+
             for (let i = 0; i < data.length; i++) {
-                var taskCard = $("<div class = taskCard>");
-                var taskTitle = $("<p class='taskTitle'>");
-                var taskPhoto = $("<img src='../images/goodjob.jfif'>");
-                var taskDescript = $("<p class='taskDescript'>");
-                var addBtn = $("<button class='addBtn'>");
-                var deleteBtn = $("<button class='deleteBtn'>");
-                addBtn.text("add");
-                addBtn.val([data[i].id, data[i].confirmation])
-                deleteBtn.text("delete");
-                taskDescript.text(data[i].description);
-                taskTitle.text(data[i].name);
-                console.log
-                taskCard.append(taskTitle, taskPhoto, taskDescript, addBtn, deleteBtn);
-                taskHolder.append(taskCard)
+                console.log('task id ', data[i].TaskId);
+                $.get(`/api/task/${data[i].TaskId}`).then(function (taskData) {
+                    console.log('inside loop userTask data ', data[i]);
+                    console.log('inside loop tast data ', taskData);
+
+                    var taskCard = $("<div class = taskCard>");
+                    var taskTitle = $("<p class='taskTitle'>");
+                    var taskPhoto = $("<img src='../images/goodjob.jfif'>");
+                    var taskDescript = $("<p class='taskDescript'>");
+                    var addBtn = $("<button class='addBtn'>");
+                    var deleteBtn = $("<button class='deleteBtn'>");
+                    addBtn.text("add");
+                    //addbtn change to complete
+                    //addBtn.val([data[i].id, data[i].confirmation])
+                    deleteBtn.text("delete");
+                    taskDescript.text(taskData.description);
+                    taskTitle.text(taskData.name);
+                    console.log
+                    taskCard.append(taskTitle, taskPhoto, taskDescript, addBtn, deleteBtn);
+                    taskHolder.append(taskCard)
+                })
             }
-
-
-            // //renders new tasks for user
-            // for (let i = 0; i < data.length; i++) {
-
-
-            // }
         }).catch(error => console.log(error))
 
     });
