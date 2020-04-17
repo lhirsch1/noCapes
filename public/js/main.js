ba62d4817ed1ed54e15af7e75b9c459abb6835fe
Lukas Hirsch <hirschlukasa@gmail.com>
2020-04-16 20:48:59 -0500

    new user creation/login working

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,48 @@
 $(document).ready(function () {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
+    $.get("/api/user_data").then(function(data) {
+        $(".member-name").text(data.email);
+      });
+
+      
     $.get("/api/tasks").then(function (data) {
         console.log(data)
         const taskHolder = $('.taskHolder')
 
 
-
+        //renders new tasks for user
         for (let i = 0; i < data.length; i++) {
             var taskCard = $("<div class = taskCard>");
             var taskTitle = $("<p class='taskTitle'>");
             var taskPhoto = $("<img src='../images/goodjob.jfif'>");
             var taskDescript = $("<p class='taskDescript'>");
+            var addBtn = $("<button class='addBtn'>");
+            var deleteBtn = $("<button class='deleteBtn'>");
+            addBtn.text("add");
+            addBtn.val([data[i].CharityId,data[i].id])
+            deleteBtn.text("delete");
             taskDescript.text(data[i].description);
             taskTitle.text(data[i].name);
             console.log
-            taskCard.append(taskTitle, taskPhoto, taskDescript);
+            taskCard.append(taskTitle, taskPhoto, taskDescript, addBtn, deleteBtn);
             taskHolder.append(taskCard)
 
         }
-    });
+    }).then(function(data){
+        let addBtns = $('.addBtn')
+        //onclick to create userTask 
+        addBtns.click(function(){
+
+            console.log("this ",this.value)
+        })
+    }).catch(error => console.log(error))
+    
+    
 });
+
+
+function addTaskToList(){
+    console.log(this.value);
+
+}
--- public/js/signup.js
+++ public/js/signup.js
@@ -3,36 +3,42 @@ $(document).ready(function() {
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
+  var firstNameInput = $("input#firstNameInput");
+  var lastNameInput = $("input#lastNameInput");
+  var zipCodeInput = $("input#zipCodeInput")
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      firstName: firstNameInput.val(),
+      lastName: lastNameInput.val(),
+      zipCode: zipCodeInput.val().trim()
     };
 
     if (!userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.email, userData.password, userData.firstName, userData.lastName, userData.zipCode);
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, password, firstName, lastName, zipCode) {
     $.post("/api/signup", {
       email: email,
       password: password,
-      phone: `1231231123`,
-      photo: `hi!`,
-      social: `hi`
+      firstName: firstName,
+      lastName: lastName,
+      zipCode: zipCode
     })
       .then(function(data) {
-        window.location.replace("/members");
+        window.location.replace("/main");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
