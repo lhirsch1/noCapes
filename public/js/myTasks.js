e612922de984bd20cd61b4bfe3a35095dfd9dbb7
Lukas Hirsch <hirschlukasa@gmail.com>
2020-04-19 14:55:51 -0500

    testing heroku

--- public/js/myTasks.js
+++ public/js/myTasks.js
@@ -106,7 +106,7 @@ function getCoords(id){
         // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
     })
 }
-getCoords(1)
+getCoords(8)
 
 
 
